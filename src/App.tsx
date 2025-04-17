60585e5a84250a1319dd4c2c921972de9e8a60ad
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2025-04-17 17:28:01 -0300

    Add Tailwind CSS integration and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold text-blue-500">
+        Vite + React + Tailwind
+      </h1>
     </>
   );
 }
