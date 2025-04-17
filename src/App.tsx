502f581cc3a2b865fb38d0289fab980ddd83535f
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2025-04-17 17:43:18 -0300

    Add Test component and Vercel configuration
    - Created a new Test component in src/shared/components/test/index.tsx that displays a simple heading and paragraph.
    - Added a Vercel configuration file (vercel.json) to set up URL rewrites for the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Test from "@/shared/components/test";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold text-blue-500">
         Vite + React + Tailwind
       </h1>
+      <Test />
     </>
   );
 }
